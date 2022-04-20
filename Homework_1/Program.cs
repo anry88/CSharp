2122d1f455c71b32d80d2c4ba5492408e0393efb
Andrey Brazhnikov <anry15@mail.ru>
2022-04-20 22:55:16 +0300

    Create fish

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -8,8 +8,25 @@ namespace Homework_1
 {
     internal class Program
     {
+
+        static int taskNumber;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Добро пожаловать в домашнюю работу Бражникова Андрея для первого урока!");
+            Console.WriteLine("Для просмотра задания введите его номер");
+
+            taskNumber = int.Parse(Console.ReadLine());
+
+            switch (taskNumber)
+            {
+                case 1:
+                    Console.WriteLine(1);
+                    break;
+                default: 
+                    Console.WriteLine("default");
+                    break;
+            }
         }
     }
 }
