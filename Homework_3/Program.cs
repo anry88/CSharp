2ec4bc3602b77fc1ccea90644a6479f1a492302f
Andrey Brazhnikov <anry15@mail.ru>
2022-05-02 01:08:08 +0300

    Finished task1 а

---
+++ Homework_3/Program.cs
@@ -0,0 +1,69 @@
+﻿using Homework_3.Tasks.Task1;
+using SharedLibs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_3
+{
+    internal class Program
+    {
+        static string taskNumber;
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            bool isRunnig = true;
+
+            while (isRunnig)
+            {
+                Console.WriteLine("Для просмотра задания введите его номер от 1 до 3 и нажмите Enter, " +
+                    "для выхода введите 0");
+
+                taskNumber = Console.ReadLine();
+
+                switch (taskNumber)
+                {
+                    case "0":
+                        isRunnig = false;
+                        break;
+                    case "1":
+                        MyLibs.Greetings("Задание 1",
+                            "а) Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры.\n" +
+                            "б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса.\n" +
+                            "в) Добавить диалог с использованием switch демонстрирующий работу класса.");
+                        Task1.Start();
+                        break;
+                    case "2":
+                        MyLibs.Greetings("Задание 2",
+                            "а) С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке).\n" +
+                            " Требуется подсчитать сумму всех нечётных положительных чисел. \n" +
+                            "Сами числа и сумму вывести на экран, используя tryParse.");
+                        //Task2.CountNumbers();
+                        break;
+                    case "3":
+                        MyLibs.Greetings("Задание 3",
+                            "*Описать класс дробей — рациональных чисел, являющихся отношением двух целых чисел. \n" +
+                            "Предусмотреть методы сложения, вычитания, умножения и деления дробей. \n" +
+                            "Написать программу, демонстрирующую все разработанные элементы класса.\n" +
+                            "Добавить свойства типа int для доступа к числителю и знаменателю;\n" +
+                            "Добавить свойство типа double только на чтение, чтобы получить десятичную дробь числа;\n" +
+                            "**Добавить проверку, чтобы знаменатель не равнялся 0." +
+                            "Выбрасывать исключение ArgumentException(\"Знаменатель не может быть равен 0\"); \n" +
+                            "***Добавить упрощение дробей.");
+                        //Task3.SumOddnumber();
+                        break;
+                    default:
+                        Console.WriteLine("Кажется, вы где-то ошиблись. Попробуйте еще раз");
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ Homework_3/Tasks/Task1/Task1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_3.Tasks.Task1
+{
+    internal class Task1
+    {
+        private static Complex complex1;
+        private static Complex complex2;
+
+        public static void Start()
+        {
+            Init();
+            Menu();
+        }
+        private static void Menu()
+        {
+            bool isRunnig = true;
+
+            while (isRunnig)
+            {
+                Console.WriteLine("Введите:\n" +
+                "1 - для сложения комплексных чисел\n" +
+                "2 - для вычитания из первого комплексного числа второго\n" +
+                "3 - для умножения комплексных чисел\n" +
+                "4 - для деления первого комплексного числа на второе\n" +
+                    "0 - для выхода в главное меню");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "0":
+                        isRunnig = false;
+                        break;
+                    case "1":
+                        Console.WriteLine($"Результат сложения комплексных чисел " +
+                            $"{complex1} и {complex2} = {complex1.Plus(complex2)}");
+                        break;
+                    case "2":
+                        Console.WriteLine($"Результат вычитания из {complex1} - {complex2} =  {complex1.Minus(complex2)}");
+                        break;
+                    case "3":
+                        Console.WriteLine($"Результат умножения комплексных чисел " +
+                            $"{complex1} и {complex2} = {complex1.Multiplication(complex2)}");
+                        break;
+                    case "4":
+                        Console.WriteLine($"Результат деления {complex1} на {complex2} = {complex1.Division(complex2)}");
+                        break;
+                    default:
+                        Console.WriteLine("Кажется, вы где-то ошиблись. Попробуйте еще раз");
+                        break;
+                }
+            }
+
+            
+
+        }
+        /// <summary>
+        /// Метод для инициализации комплексных чисел
+        /// </summary>
+        private static void Init()
+        {
+            bool needInit = true;
+
+            while (needInit)
+            {
+                try
+                {
+                    Console.WriteLine("Введите действительную часть первного комплексного числа:");
+                    complex1.re = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите действительную часть первого комплексного числа:");
+                    complex1.im = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите действительную часть второго комплексного числа:");
+                    complex2.re = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите действительную часть второго комплексного числа:");
+                    complex2.im = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine($"Ваши числа:\n {complex1}\n {complex2}");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Кажется, вы где-то ошиблись. Попробуйте еще раз");
+
+                }
+
+                needInit = false;
+            }            
+
+        }
+    }
+}
