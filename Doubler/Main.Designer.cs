41297e48160951eb7566be63038e6bd1702aac22
Andrey Brazhnikov <anry15@mail.ru>
2022-06-07 23:17:33 +0300

    copy from lesson

---
+++ Doubler/Main.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Doubler
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonMultiply = new System.Windows.Forms.Button();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.labelComputerNumber = new System.Windows.Forms.Label();
+            this.labelUserNubmer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonReset.Location = new System.Drawing.Point(303, 35);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(126, 61);
+            this.buttonReset.TabIndex = 0;
+            this.buttonReset.Text = "Новая игра";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // buttonMultiply
+            // 
+            this.buttonMultiply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonMultiply.Location = new System.Drawing.Point(303, 102);
+            this.buttonMultiply.Name = "buttonMultiply";
+            this.buttonMultiply.Size = new System.Drawing.Size(126, 61);
+            this.buttonMultiply.TabIndex = 1;
+            this.buttonMultiply.Text = "x2";
+            this.buttonMultiply.UseVisualStyleBackColor = true;
+            this.buttonMultiply.Click += new System.EventHandler(this.buttonMultiply_Click);
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPlus.Location = new System.Drawing.Point(303, 169);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(126, 61);
+            this.buttonPlus.TabIndex = 2;
+            this.buttonPlus.Text = "+1";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // labelComputerNumber
+            // 
+            this.labelComputerNumber.AutoSize = true;
+            this.labelComputerNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelComputerNumber.Location = new System.Drawing.Point(65, 76);
+            this.labelComputerNumber.Name = "labelComputerNumber";
+            this.labelComputerNumber.Size = new System.Drawing.Size(151, 20);
+            this.labelComputerNumber.TabIndex = 3;
+            this.labelComputerNumber.Text = "Получите число:";
+            // 
+            // labelUserNubmer
+            // 
+            this.labelUserNubmer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelUserNubmer.AutoSize = true;
+            this.labelUserNubmer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelUserNubmer.Location = new System.Drawing.Point(76, 143);
+            this.labelUserNubmer.Name = "labelUserNubmer";
+            this.labelUserNubmer.Size = new System.Drawing.Size(140, 20);
+            this.labelUserNubmer.TabIndex = 4;
+            this.labelUserNubmer.Text = "Текущее число:";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 261);
+            this.Controls.Add(this.labelUserNubmer);
+            this.Controls.Add(this.labelComputerNumber);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.buttonMultiply);
+            this.Controls.Add(this.buttonReset);
+            this.MaximumSize = new System.Drawing.Size(500, 400);
+            this.MinimumSize = new System.Drawing.Size(475, 300);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Удвоитель";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonMultiply;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Label labelComputerNumber;
+        private System.Windows.Forms.Label labelUserNubmer;
+    }
+}
+
---
+++ Doubler/Main.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Doubler
+{
+    public partial class Main : Form
+    {
+        private Random random = new Random();
+        private int computerNumber;
+        private int userNumber;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            UpdateGameState(userNumber, random.Next(20));
+        }
+
+        private void UpdateGameState(int userNumber)
+        {
+            labelUserNubmer.Text = $"Текущее число: {userNumber}";
+        }
+
+        private void UpdateGameState(int userNumber, int computerNumber)
+        {
+            UpdateGameState(userNumber);
+            this.computerNumber = computerNumber;
+            labelComputerNumber.Text = $"Получите число: {computerNumber}";
+        }
+
+
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            userNumber = 0;
+            UpdateGameState(userNumber, random.Next(20));
+        }
+
+        private void buttonMultiply_Click(object sender, EventArgs e)
+        {
+            userNumber = userNumber * 2;
+            //UpdateGameState(userNumber);
+
+            UpdateGameState(userNumber *= 2);
+            CheckWin();
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            //userNumber = userNumber + 1;
+            //UpdateGameState(userNumber);
+            UpdateGameState(++userNumber);
+            CheckWin();
+        }
+
+        private void CheckWin()
+        {
+            if (userNumber == computerNumber)
+            {
+                MessageBox.Show("Вы успешно завершили игру!", "Удвоитель",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (MessageBox.Show("Желаете сыграть еще раз?", "Удвоитель", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    userNumber = 0;
+                    UpdateGameState(userNumber, random.Next(20));
+                }
+                else
+                {
+                    Close();
+                }
+            }
+        }
+    }
+}
